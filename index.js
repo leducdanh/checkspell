84a0de1cfc1f870dc916c176de8bffd081bbe5d6
danhle <tntdanhle@gmail.com>
2019-06-24 21:46:41 +0700

    upd src

--- index.js
+++ index.js
@@ -8,33 +8,44 @@ var app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-
-app.get("/spellchecker/:text", function (req, response) {
+InitDatatable = async () => {
+    
+}
+app.get("/spellchecker/:text", async function (req, response) {
     let text = req.params.text
     let tokenizer = new natural.TreebankWordTokenizer();
     let ArrayWord = tokenizer.tokenize(text)
-    ArrayWord.forEach((e, index) => {
-        let word = "" + e
-        SpellChecker.getDictionary("en-US", function (err, dictionary) {
-            if (!err) {
-                var misspelled = !dictionary.spellCheck(word);
-                if (misspelled) {
-                    var suggestions = dictionary.getSuggestions(word);
-                    let cose = 0
-                    let str = ""
-                    for (let i = 0; i < suggestions.length; i++) {
-                        if (natural.JaroWinklerDistance(suggestions[i], word) > cose) {
-                            cose = natural.JaroWinklerDistance(suggestions[i], word)
-                            str = suggestions[i]
+    var p3 = new Promise((resolve, reject) => {
+        ArrayWord.forEach((e, index) => {
+            let word = "" + e
+            SpellChecker.getDictionary("en-US", (err, dictionary) => {
+                if (!err) {
+                    var misspelled = !dictionary.spellCheck(word);
+                    if (misspelled) {
+                        console.log(word);
+                        var suggestions = dictionary.getSuggestions(word);
+                        let cose = 0
+                        let str = ""
+                        for (const item of suggestions) {
+                            if (natural.JaroWinklerDistance(item, word) > cose) {
+                                cose = natural.JaroWinklerDistance(item, word)
+                                str = item
+                            }
                         }
+                        text = text.replace(word, str)
                     }
-                    text = text.replace(word, str)
                 }
-            }
-            if (index === ArrayWord.length-1) {
-                response.json(text)
-            }
+                if (index === ArrayWord.length - 1) {
+                    // response.json(text)
+                    resolve(text);
+                }
+            });
         });
+    });
+
+    await Promise.all([p3]).then(values => {
+        console.log(values);
+        response.json(text)
     });
 });
 
