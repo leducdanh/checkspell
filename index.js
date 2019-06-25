82d37c724dd8eab20217bf3b002c7fdc231d18df
danhle <tntdanhle@gmail.com>
2019-06-25 23:21:45 +0700

    upd src

--- index.js
+++ index.js
@@ -24,13 +24,58 @@ const ChangeMsg = (word, text) => {
                         }
                     }
                     text = text.replace(word, str)
-                    
+
                 }
                 resolve(text)
             }
         })
     })
 }
+
+const Suggestions = (word) => {
+    return new Promise((resolve, reject) => {
+        return SpellChecker.getDictionary("en-US", (err, dictionary) => {
+            if (!err) {
+                var misspelled = !dictionary.spellCheck(word);
+                if (misspelled) {
+                    var suggestions = dictionary.getSuggestions(word);
+
+                    resolve("" + suggestions)
+
+                }
+            }
+        })
+    })
+}
+
+app.get("/suggestions/:text", async function (req, response) {
+    let text = req.params.text
+    let tokenizer = new natural.TreebankWordTokenizer();
+    let ArrayWord = tokenizer.tokenize(text)
+    let lstWord = []
+    let lstSuggesByWord = []
+    let strResult = ""
+    for (const e of ArrayWord) {
+        let word = "" + e
+        let strSugges = ""
+        strSugges = await Suggestions(word)
+        if (strSugges !== ""){
+            lstWord.push(word)
+            lstSuggesByWord.push(strSugges)
+        }
+    }
+
+    let index = 0
+    for (const e of lstWord) {
+        let word = "" + e
+        strResult += `"` + word + `":` + `"` + lstSuggesByWord[index].toString() + `",`
+        index++
+    }
+    strResult = strResult.substr(0, strResult.length-1)
+    let t = JSON.parse(("{" + strResult + "}"))
+    response.json(t)
+
+});
 
 app.get("/spellchecker/:text", async function (req, response) {
     let text = req.params.text
